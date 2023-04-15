2c7b0ec9352e2d10be4eaab42a1d3ac7b9329fa0
mohitshahi <moheetshahi92@gmail.com>
2023-04-15 16:35:09 +0545

    added template and plugin link in navigation

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php
 
+use core\plugininfo\local;
+
 require_once('../../config.php');
-require_once($CFG->dirroot . '/local/testplugin/locallib.php');
+require_once($CFG->dirroot . '/local/testplugin/lib.php');
 
 $context = context_system::instance();
 $PAGE->set_context($context);
-
+// $PAGE->set_theme('classic');
 $PAGE->set_url(new moodle_url('/local/testplugin/index.php'));
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading(get_string('pluginname', 'local_testplugin'));
-
 echo $OUTPUT->header();
 
-$date = time();
+$date = userdate(time());
 
-echo userdate($date) . '<br>';
-echo $USER->country . '<br>';
+// echo var_dump(new moodle_url('/local/testplugin/index.php'));
+// echo var_dump($PAGE->context) . '<br>';
+
 
 if(isloggedin()){
-    echo local_greeting_get_greeting($USER);
+    $greetingMessage = local_greeting_get_greeting($USER);
 }
 else{
-    echo get_string('greetinguser', 'local_testpugin');
+    $greetingMessage = get_string('greetinguser', 'local_testpugin');
     // require_login();
 }
+
+$greetingTemplateContext = ['message'=>$greetingMessage, 'date'=>$date, 'country'=>$USER->country];
+
+echo $OUTPUT->render_from_template('local_testplugin/greeting', $greetingTemplateContext);
 
 echo $OUTPUT->footer();
---
+++ lib.php
@@ -0,0 +1,47 @@
+<?php
+
+use function PHPSTORM_META\type;
+
+function local_greeting_get_greeting($user){
+    if($user == null){
+        return get_string('greetinguser', 'local_testplugin');
+    }
+
+    $country = $user->country;
+    switch($country){
+        case 'ES':
+            $langstr = 'greetingloggedinuser_es';
+            break;
+        case 'NP':
+            $langstr = 'greetingloggedinuser_np';
+            break;
+        case 'IN':
+            $langstr = 'greetingloggedinuser_in';
+            break;
+        default:
+            $langstr = 'greetingloggedinuser';
+            break;
+    }
+    return get_string($langstr, 'local_testplugin', fullname($user));
+}
+//diaplays message on top 
+// function local_testplugin_before_footer(){
+//     \core\notification::add(message:'Hello from test plugin', level:\core\output\notification::NOTIFY_INFO);
+// }
+
+function local_testplugin_extend_navigation_frontpage(navigation_node $frontpage){
+    $frontpage->add(get_string('pluginname', 'local_testplugin'), new moodle_url('/local/testplugin/index.php'));
+}
+
+function local_testplugin_extend_navigation(global_navigation $root) {
+    $node = navigation_node::create(
+        get_string('pluginname', 'local_testplugin'),
+        new moodle_url('/local/testplugin/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+
+    $root->add_node($node);
+}
